fc3c998d6beae32a1b0a935ead80467ccac9b5d0
JGTV <22498524+JonnygamingTv@users.noreply.github.com>
2026-04-13 20:04:09 +0200

    Check Rocket.API

--- Rocket/Rocket.API/IRocketPermissionsProvider.cs
+++ Rocket/Rocket.API/IRocketPermissionsProvider.cs
@@ -10,7 +10,7 @@ namespace Rocket.API
     {
         public static bool HasPermission(this IRocketPermissionsProvider rocketPermissionProvider,IRocketPlayer player, string permission)
         {
-            return rocketPermissionProvider.HasPermission(player, new List<string>() { permission });
+            return rocketPermissionProvider.HasPermission(player, new List<string>(1) { permission });
         }
 
         public static bool HasPermission(this IRocketPermissionsProvider rocketPermissionProvider, IRocketPlayer player, IRocketCommand command)
@@ -41,7 +41,7 @@ namespace Rocket.API
     {
         bool HasPermission(IRocketPlayer player, List<string> requestedPermissions);
         //bool HasPermission(IRocketPlayer player, HashSet<string> requestedPermissions);
-
+        // Seems like modifying interface will break some plugins like ShimmysAdminTools
         List<RocketPermissionsGroup> GetGroups(IRocketPlayer player, bool includeParentGroups);
         List<Permission> GetPermissions(IRocketPlayer player);
         List<Permission> GetPermissions(IRocketPlayer player, List<string> requestedPermissions);
